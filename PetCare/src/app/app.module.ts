9daba6dc37ab8a8935d4ed3e106fd951d8f3662f
Vladyslava <kurbanova.vladislava@gmail.com>
2018-11-03 13:48:16 +0200

    small update

--- PetCare/src/app/app.module.ts
+++ PetCare/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShelterComponent } from './components/shelter/shelter.component';
+import { MatCardModule } from '@angular/material/card';
+import { PetComponent } from './components/pet/pet.component';
+import { PetsListComponent } from './components/pets-list/pets-list.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShelterComponent,
+    PetComponent,
+    PetsListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
